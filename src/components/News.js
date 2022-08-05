207a69066b6d64f5c72f96b1b929bdb6ed0e1516
Roman Farooq <TheRomanFarooq@gmail.com>
2022-08-05 23:39:34 +0500

    Attached newsApi to article

--- src/components/News.js
+++ src/components/News.js
@@ -1,70 +1,25 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import NewsItem from "./NewsItem";
 
 function News() {
-  const articles = [
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "PCB hands Umar Akmal three-year ban from all cricket | ESPNcricinfo.com",
-      description:
-        "Penalty after the batsman pleaded guilty to not reporting corrupt approaches | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/29103103/pcb-hands-umar-akmal-three-year-ban-all-cricket",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1099495_800x450.jpg",
-      publishedAt: "2020-04-27T11:41:47Z",
-      content:
-        "Umar Akmal's troubled cricket career has hit its biggest roadblock yet, with the PCB handing him a ban from all representative cricket for three years after he pleaded guilty of failing to report det… [+1506 chars]",
-    },
-    {
-      source: { id: "espn-cric-info", name: "ESPN Cric Info" },
-      author: null,
-      title:
-        "What we learned from watching the 1992 World Cup final in full again | ESPNcricinfo.com",
-      description:
-        "Wides, lbw calls, swing - plenty of things were different in white-ball cricket back then | ESPNcricinfo.com",
-      url: "http://www.espncricinfo.com/story/_/id/28970907/learned-watching-1992-world-cup-final-full-again",
-      urlToImage:
-        "https://a4.espncdn.com/combiner/i?img=%2Fi%2Fcricket%2Fcricinfo%2F1219926_1296x729.jpg",
-      publishedAt: "2020-03-30T15:26:05Z",
-      content:
-        "Last week, we at ESPNcricinfo did something we have been thinking of doing for eight years now: pretend-live ball-by-ball commentary for a classic cricket match. We knew the result, yes, but we tried… [+6823 chars]",
-    },
-  ];
+  
+  const [article, setArticle] = useState([]);
+  // const [loading, setLoading] = useState(true);use
+  // const [page, setPage] = useState(1);
+  // const [totalResults, setTotalResults] = useState(0);
 
-  const [info, setInfo] = useState({
-    articles: articles,
-    loading: true,
-  });
+  const updateNews = async () => {
+    const url =
+      "https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c";
+    const data = await fetch(url);
+    const parsedData = await data.json();
+    console.log(parsedData);
+    setArticle(parsedData.articles);
+  };
+
+  useEffect(() => {
+    updateNews();
+  }, []);
 
   return (
     <>
@@ -72,13 +27,18 @@ function News() {
         NewsMonkey - Top Headlines
       </h2>
       <div className="d-flex align-content-around flex-wrap">
-        {info.articles.map((element) => {
+        {article.map((element) => {
           return (
             <NewsItem
               key={element.url}
-              title={element.title}
-              description={element.description}
-              imageUrl={element.urlToImage}
+              title={element.title ? element.title : ""}
+              description={element.description ? element.description : ""}
+              imageUrl={
+                element.urlToImage
+                  ? element.urlToImage
+                  : "https://image.cnbcfm.com/api/v1/image/107000211-1642009706804-gettyimages-1011792700-mon1158118.jpeg?v=1642009864&w=1920&h=1080"
+              }
+              newsUrl={element.url}
             />
           );
         })}
--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -2,16 +2,16 @@ import React from "react";
 
 function NewsItem(props) {
   
-  const { title, description, imageUrl, key } = props;
+  const { title, description, imageUrl, newsUrl } = props;
 
   return (
     <div style={{ margin: "0 4rem" }}>
       <div className="card my-3" style={{ width: "18rem" }}>
         <img src={imageUrl} className="card-img-top" alt={title} />
         <div className="card-body">
-          <h5 className="card-title">{title.slice(0, 74)}</h5>
+          <h5 className="card-title">{title}</h5>
           <p className="card-text">{description.slice(0, 105)}</p>
-          <a href={key} target="_blank" className="btn btn-sm btn-primary">
+          <a href={newsUrl} target="_blank" className="btn btn-sm btn-primary">
             Read More...
           </a>
         </div>
