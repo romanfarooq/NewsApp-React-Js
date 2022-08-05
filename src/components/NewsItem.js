4fd873dd5e5d8354d68b68822afc95d5d55b55d3
Roman Farooq <TheRomanFarooq@gmail.com>
2022-08-05 19:18:31 +0500

    Added NewsItem card with sample data

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -1,20 +1,19 @@
 import React from "react";
 
-function NewsItem() {
+function NewsItem(props) {
+  const { title, description, imageUrl, key } = props;
+
   return (
-    <div className="card my-3" style={{ width: "18rem" }}>
-      <img src="..." className="card-img-top" alt="..." />
-      <div className="card-body">
-        <h5 className="card-title">Card title</h5>
-        <p className="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="/#" className="btn btn-primary">
-          Go somewhere
-        </a>
+    <div className="card my-3" style={{ width: "18rem"}}>
+        <img src={imageUrl} className="card-img-top" alt={title} />
+        <div className="card-body">
+          <h5 className="card-title">{title.slice(0, 74)}</h5>
+          <p className="card-text">{description.slice(0, 105)}</p>
+          <a href={key} target="_blank" className="btn btn-sm btn-primary">
+            Read More...
+          </a>
+        </div>
       </div>
-    </div>
   );
 }
 
