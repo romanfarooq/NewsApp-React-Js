67e4e01bd923eb2dd88202b51646d3e6a24a534d
Roman Farooq <TheRomanFarooq@gmail.com>
2022-08-06 19:49:29 +0500

    Chnages in useed

--- src/components/News.js
+++ src/components/News.js
@@ -8,45 +8,26 @@ function News(props) {
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
 
-  // const updateNews = async () => {
-  //   const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`;
-  //   const data = await fetch(url);
-  //   const parsedData = await data.json();
-  //   setArticle(parsedData.articles);
-  //   //setTotalResults(parsedData.totalResults);
-  // };
-
   useEffect(() => {
-    const seteNews = async () => {
-      const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`;
-      setLoading(true);
-      const data = await fetch(url);
-      const parsedData = await data.json();
-      setArticle(parsedData.articles);
-      setTotalResults(parsedData.totalResults);
-      setLoading(false);
-    };
-    seteNews();
-  }, []);
-
-  const handlePrevious = async () => {
-    setPage(page - 1);
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page - 1}`;
     setLoading(true);
-    const data = await fetch(url);
-    const parsedData = await data.json();
-    setArticle(parsedData.articles);
+    fetch(
+      `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`
+    )
+      .then((response) => response.json())
+      .then((data) => setArticle(data.articles))
+      .then((data) => setTotalResults(data.totalResults))
+      .catch((err) => {
+        console.log(err);
+      });
     setLoading(false);
+  }, [page]);
+
+  const handlePrevious = () => {
+    setPage(page - 1);
   };
 
-  const handleNext = async () => {
+  const handleNext = () => {
     setPage(page + 1);
-    const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page + 1}`;
-    setLoading(true);
-    const data = await fetch(url);
-    const parsedData = await data.json();
-    setArticle(parsedData.articles);
-    setLoading(false);
   };
 
   return (
@@ -54,21 +35,22 @@ function News(props) {
       <h2 className="my-3 text-center">NewsMonkey - Top Headlines</h2>
       {loading && <Spinner />}
       <div className="d-flex align-content-around flex-wrap">
-        {!loading && article.map((element) => {
-          return (
-            <NewsItem
-              key={element.url}
-              title={element.title ? element.title : ""}
-              description={element.description ? element.description : ""}
-              imageUrl={
-                element.urlToImage
-                  ? element.urlToImage
-                  : "https://image.cnbcfm.com/api/v1/image/107000211-1642009706804-gettyimages-1011792700-mon1158118.jpeg?v=1642009864&w=1920&h=1080"
-              }
-              newsUrl={element.url}
-            />
-          );
-        })}
+        {!loading &&
+          article.map((element) => {
+            return (
+              <NewsItem
+                key={element.url}
+                title={element.title ? element.title : ""}
+                description={element.description ? element.description : ""}
+                imageUrl={
+                  element.urlToImage
+                    ? element.urlToImage
+                    : "https://image.cnbcfm.com/api/v1/image/107000211-1642009706804-gettyimages-1011792700-mon1158118.jpeg?v=1642009864&w=1920&h=1080"
+                }
+                newsUrl={element.url}
+              />
+            );
+          })}
       </div>
       <div
         className="d-flex justify-content-between"
@@ -77,7 +59,7 @@ function News(props) {
         <button
           type="button"
           className="btn btn-dark"
-          disabled={page === 1}
+          disabled={page <= 1}
           onClick={handlePrevious}
         >
           &larr; Previous
@@ -85,7 +67,7 @@ function News(props) {
         <button
           type="button"
           className="btn btn-dark px-4"
-          disabled={page === Math.ceil(totalResults / props.pageSize)}
+          disabled={page + 1 > Math.ceil(totalResults / props.pageSize)}
           onClick={handleNext}
         >
           Next &rarr;
