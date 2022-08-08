1ba6510c1a87ed7af72e978baf3ae6775f3a9225
Roman Farooq <TheRomanFarooq@gmail.com>
2022-08-09 02:51:47 +0500

    Added infinity scroll

--- src/components/News.js
+++ src/components/News.js
@@ -1,86 +1,114 @@
 import React, { useState, useEffect } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
 import Spinner from "./Spinner";
 
 function News(props) {
   const [article, setArticle] = useState([]);
-  const [loading, setLoading] = useState(false);
+  // const [loading, setLoading] = useState(false);
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
   const [error, setError] = useState(null);
 
-  useEffect(() => {
-    const updateNews = async () => {
-      try {
-        document.title = `NewsMonkey - ${capitalize(props.category)}`;
-        setLoading(true);
-        const response = await fetch(
-          `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`
+  const updateNews = async () => {
+    try {
+      document.title = `NewsMonkey - ${capitalize(props.category)}`;
+      // setLoading(true);
+      const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`
+      );
+      if (!response.ok) {
+        throw new Error(
+          `This is an HTTP error: The status is ${response.status}`
         );
-        if (!response.ok) {
-          throw new Error(
-            `This is an HTTP error: The status is ${response.status}`
-          );
-        }
-        let actualData = await response.json();
-        setArticle(actualData.articles);
-        setTotalResults(actualData.totalResults);
-        setError(null);
-      } catch (err) {
-        setError(err.message);
-        setArticle([]);
-        setTotalResults(0);
-      } finally {
-        setLoading(false);
       }
-    };
+      let actualData = await response.json();
+      console.log(article);
+      setArticle(article.concat(actualData.articles));
+      setTotalResults(actualData.totalResults);
+      setError(null);
+    } catch (err) {
+      setError(err.message);
+      setArticle([]);
+      setTotalResults(0);
+    } finally {
+      // setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    setArticle([]);
     updateNews();
-  }, [page, props.country, props.category, props.pageSize]);
+    // eslint-disable-next-line
+  }, []);
 
   const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
-  }
-
-  const handlePrevious = async () => {
-    setPage(page - 1);
   };
 
-  const handleNext = async () => {
+  const fetchMoreData = async () => {
+    // a fake async api call like which sends
+    // 20 more records in 1.5 secs
     setPage(page + 1);
+    updateNews();
+    // setTimeout(() => {
+    //   this.setState({
+    //     items: this.state.items.concat(Array.from({ length: 20 }))
+    //   });
+    // }, 1500);
   };
+
+  // const handlePrevious = async () => {
+  //   setPage(page - 1);
+  // };
+
+  // const handleNext = async () => {
+  //   setPage(page + 1);
+  // };
 
   return (
     <>
-      <div className="container my-3">
-        <h2 className="text-center my-3">NewsMonkey - Top {capitalize(props.category)} Headlines</h2>
-        {loading && <Spinner />}
-        <div className="row">
-          {!loading && error ? (
-            <h2 className="text-center my-5">{error}</h2>
-          ) : (
-            !loading && article.map((element) => {
-              return (
-                <div className="col-md-4" key={element.url}>
-                  <NewsItem
-                    title={element.title ? element.title : ""}
-                    description={element.description ? element.description : ""}
-                    imageUrl={
-                      element.urlToImage
-                        ? element.urlToImage
-                        : "https://image.cnbcfm.com/api/v1/image/107000211-1642009706804-gettyimages-1011792700-mon1158118.jpeg?v=1642009864&w=1920&h=1080"
-                    }
-                    newsUrl={element.url}
-                    author={element.author ? element.author : "Unknown"}
-                    date={new Date(element.publishedAt).toGMTString()}
-                    source={element.source.name}
-                  />
-                </div>
-              );
-            })
-          )}
+      <h2 className="text-center my-3">
+        NewsMonkey - Top {capitalize(props.category)} Headlines
+      </h2>
+      <InfiniteScroll
+        dataLength={article.length}
+        next={fetchMoreData}
+        hasMore={article.length !== totalResults}
+        loader={<Spinner />}
+      >
+        <div className="container">
+          <div className="row">
+            {error ? (
+              <h2 className="text-center my-5">{error}</h2>
+            ) : (
+              article.map((element) => {
+                return (
+                  <div className="col-md-4" key={element.url}>
+                    <NewsItem
+                      title={element.title ? element.title : ""}
+                      description={
+                        element.description ? element.description : ""
+                      }
+                      imageUrl={
+                        element.urlToImage
+                          ? element.urlToImage
+                          : "https://image.cnbcfm.com/api/v1/image/107000211-1642009706804-gettyimages-1011792700-mon1158118.jpeg?v=1642009864&w=1920&h=1080"
+                      }
+                      newsUrl={element.url}
+                      author={element.author ? element.author : "Unknown"}
+                      date={new Date(element.publishedAt).toGMTString()}
+                      source={element.source.name}
+                    />
+                  </div>
+                );
+              })
+            )}
+          </div>
         </div>
-        {!loading && (
+      </InfiniteScroll>
+      {/* {!loading && (
           <div className="d-flex justify-content-between">
             <button
               type="button"
@@ -103,8 +131,7 @@ function News(props) {
               Next &rarr;
             </button>
           </div>
-        )}
-      </div>
+        )} */}
     </>
   );
 }
--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -3,10 +3,7 @@ import loading from "./loading.gif";
 
 function Spinner() {
   return (
-    <div
-      className="d-flex align-items-center justify-content-center"
-      style={{ height: "60vh" }}
-    >
+    <div className="d-flex justify-content-center">
       <img src={loading} alt="loading" />
     </div>
   );
