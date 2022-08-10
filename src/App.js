2b2294290930a92b584da900d315d018420b02ab
Roman Farooq <TheRomanFarooq@gmail.com>
2022-08-10 21:46:15 +0500

    added keys to router

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Navbar />}>
-          <Route index element={<News category="general"/>} />
-          <Route path="/business" element={<News category="business" />} />
-          <Route path="/entertainment" element={<News category="entertainment" />} />
-          <Route path="/health" element={<News category="health" />} />
-          <Route path="/science" element={<News category="science" />} />
-          <Route path="/sports" element={<News category="sports" />} />
-          <Route path="/technology" element={<News category="technology" />} />
+          <Route index element={<News category="general" key="general"/>} />
+          <Route path="/business" element={<News category="business" key="business"/>} />
+          <Route path="/entertainment" element={<News category="entertainment" key="entertainment"/>} />
+          <Route path="/health" element={<News category="health" key="health"/>} />
+          <Route path="/science" element={<News category="science" key="science"/>} />
+          <Route path="/sports" element={<News category="sports" key="sports"/>} />
+          <Route path="/technology" element={<News category="technology" key="technology"/>} />
         </Route>
       </Routes>
     </BrowserRouter>
--- src/components/News.js
+++ src/components/News.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect } from "react";
 import InfiniteScroll from "react-infinite-scroll-component";
 import PropTypes from "prop-types";
 import NewsItem from "./NewsItem";
@@ -9,20 +9,14 @@ function News(props) {
   const [page, setPage] = useState(1);
   const [totalResults, setTotalResults] = useState(0);
   const [error, setError] = useState(null);
-  const prevCategory = useRef(props.category);
 
   useEffect(() => {
     const abortController = new AbortController();
     const updateNews = async () => {
       try {
-        if (prevCategory.current !== props.category) {
-          setArticle([]);
-          setPage(1);
-          setTotalResults(0);
-        }
         document.title = `NewsMonkey - ${capitalize(props.category)}`;
         const response = await fetch(
-          `https://newsapi.org/v2/top-headlines?country=${props.country}&category=${props.category}&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=${props.pageSize}&page=${page}`,
+          `https://newsapi.org/v2/top-headlines?country=us&category=${props.category}&apiKey=fd9d876538e0440986ae848fcfcbc24c&pageSize=18&page=${page}`,
           { signal: abortController.signal }
         );
         if (!response.ok) {
@@ -31,7 +25,7 @@ function News(props) {
           );
         }
         let actualData = await response.json();
-        setArticle([...article, ...actualData.articles]);
+        setArticle(article.concat(actualData.articles));
         setTotalResults(actualData.totalResults);
         setError(null);
       } catch (err) {
@@ -41,16 +35,14 @@ function News(props) {
           setTotalResults(0);
           setPage(1);
         }
-      } finally {
-        prevCategory.current = props.category;
       }
     };
     updateNews();
     return () => {
       abortController.abort();
     };
-    // eslint-disable-next-line
-  }, [props.category, page]);
+    //eslint-disable-next-line 
+  }, [page]);
 
   const capitalize = (string) => {
     return string.charAt(0).toUpperCase() + string.slice(1);
@@ -106,17 +98,11 @@ function News(props) {
 }
 
 News.defaultProps = {
-  country: "us",
   category: "general",
-  pageSize: 18,
-  page: 1,
 };
 
 News.propTypes = {
-  country: PropTypes.string.isRequired,
   category: PropTypes.string.isRequired,
-  pageSize: PropTypes.number.isRequired,
-  page: PropTypes.number.isRequired,
 };
 
 export default News;
